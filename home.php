2659110ad2f97a655b09f985e7e19e7f2c1c7467
Kosuke Shinto <luby@outlook.jp>
2024-02-29 10:42:00 +0900

    ホームから店舗情報を削除

--- home.php
+++ home.php
@@ -125,30 +125,6 @@
     </div>
   </section>
 
-  <section class="offices">
-    <h2 id="office">店舗情報</h2>
-    <ul class="office">
-      <li>
-        <h3>読売センター三田南<span>本社</span></h3>
-        <address><span class="zip-code">〒669-1535</span>兵庫県三田市南が丘1-52-11</address>
-        <dl class="phone-">
-          <dt>電話番号</dt>
-          <dd>079-563-2558</dd>
-        </dl>
-        <div class="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1511.1187593813827!2d135.2176259824886!3d34.88116651646679!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60006404c8590407%3A0x456472a083ca8619!2z44CSNjY5LTE1MzUg5YW15bqr55yM5LiJ55Sw5biC5Y2X44GM5LiY77yR5LiB55uu77yV77yS4oiS77yR77yR!5e0!3m2!1sja!2sjp!4v1706247670482!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-      </li>
-      <li>
-        <h3>読売センター新三田<span>配達拠点</span></h3>
-        <address><span class="zip-code">〒669-</span>兵庫県三田市すずかけ台4-14-1</address>
-        <div class="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1635.9905383649066!2d135.20262153878028!3d34.906924392846534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600065b642ba62bd%3A0x7f4322df15c85055!2z6Kqt5aOy44K744Oz44K_44O8IOaWsOS4ieeUsA!5e0!3m2!1sja!2sjp!4v1706247761242!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-      </li>
-    </ul>
-  </section>
-
   <section class="sustainability">
     <img class=""
       src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sustainability.jpeg"
