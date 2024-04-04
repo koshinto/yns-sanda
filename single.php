9b3036d02113f49746b9fc7dd6c460b80fc29400
Kosuke Shinto <luby@outlook.jp>
2024-04-04 17:49:20 +0900

    ch: single.phpのページタイトル対応

--- single.php
+++ single.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 <main id="page" class="page">
-  <?php
-    get_template_part( 'template-parts/breadcrumb' );
-    get_template_part( 'template-parts/pagetitle' );
-  ?>
+  <div class="page-title">
+    <?php
+      get_template_part( 'template-parts/breadcrumb' );
+      get_template_part( 'template-parts/pagetitle' );
+    ?>
+  </div>
   <div class="page-content">
     <?php if ( have_posts() ): while ( have_posts() ): the_post(); 
       the_content();
