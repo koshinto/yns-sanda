fbb5caaf675d870587ebc3ce8e960c64c4baf473
Kosuke Shinto <luby@outlook.jp>
2024-05-24 09:51:20 +0900

    フッターのレスポンシブ化

--- footer.php
+++ footer.php
@@ -1,37 +1,31 @@
 <footer id="footer">
-  <nav class="sitemap">
+  <nav id="footer-sitemap" class="sitemap">
     <?php 
-    wp_nav_menu( array(
-      'theme_location' => 'place_sitemap',
-      'container' => false
-    ) )
+      wp_nav_menu(array(
+        'theme_location' => 'place_sitemap',
+        'container' => false,
+        'menu_class' => 'sitemap-menu'
+      ));
+      wp_nav_menu(array(
+        'theme_location' => 'place_legal',
+        'container' => false,
+        'menu_class' => 'legal-menu'
+      ));
     ?>
   </nav>
-  <div class="footer-info">
+  <div class="footer-inner">
     <div class="footer-logo">
-      <p class="site-description"><?php bloginfo( 'description' ); ?></p>
-      <h2 >ワイエヌエス三田</h2>
+      <h2 class="corporate">株式会社ワイエヌエス三田</h2>
     </div>
-    <nav class="sns-icon">
-      <?php
-      wp_nav_menu( array(
-        'theme_location' => 'place_social_service',
-        'container' => false
-      ) )
-      ?>
-    </nav>
-    <div class="modest-text">
+    <div class="address">
+      <span class="footer-zipcode">〒669-1535</span>
+      <span>兵庫県三田市南が丘1−52−11</span>
+      <span class="footer-tel">TEL： 079-563-2558</span>
+    </div>
+    <div class="law-contents">
       <p class="copyright"><small>&copy; 2024 YNS-Sanda.co,ltd</small></p>
-      <nav class="legal">
-        <?php
-        wp_nav_menu( array(
-          'theme_location' => 'place_legal',
-          'container' => false
-        ))
-        ?>
-      </nav>
     </div>
-    </div>
+  </div>
 </footer>
 <?php wp_footer(); ?>
 <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/js/menu.js"></script>
