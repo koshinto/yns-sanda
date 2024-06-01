851eb9bc141602205adf1bd08c99955db15ed887
Kosuke Shinto <luby@outlook.jp>
2024-06-01 12:51:58 +0900

    記事のページを追加

--- single.php
+++ single.php
@@ -1,24 +1,27 @@
 <?php get_header(); ?>
-<main id="page" class="page page-post">
-  <div class="page-title">
-    <?php
-      get_template_part( 'template-parts/breadcrumb' );
-      get_template_part( 'template-parts/pagetitle' );
-    ?>
-  </div>
-  <div class="page-content">
-    <div class="page-content-margin">
-      <div class="page-content-body">
-        <?php
-          if ( have_posts() ): while ( have_posts() ): the_post(); 
-            the_content();
-          endwhile; endif;
-        ?>
-      </div>
+<main id="single" class="single single-common">
+  <article class="contents corporate">
+    <?php get_template_part('template-parts/pagetitle'); ?>
+    <div class="page-container">
+      <?php
+        if ( have_posts() ): while ( have_posts() ): the_post();
+          $author_email = get_the_author_meta( 'user_email' );
+          the_content();
+      ?>
+      <section id="writer">
+        <div class="author-icon"><?php echo get_avatar( $author_email ); ?></div>
+        <div class="author-profile">
+          <h3 class="author-name"><?php the_author(); ?></h3>
+          <p class="author-description"><?php echo get_the_author_meta( 'description' ); ?></p>
+        </div>
+      </section>
+      <?php
+        endwhile; endif;
+      ?>
     </div>
-    </div>
+  </article>
+
 </main>
 <?php
-  dynamic_sidebar( 'bottom-widget-area' );
   get_footer();
 ?>
